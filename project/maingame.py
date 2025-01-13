8e063254bdc8c83c4c483b7d292fdd862072da60
Anandaa69 <anandasinlapachote0@gmail.com>
2025-01-13 15:25:16 +0700

    Create Outline App

---
+++ project/maingame.py
@@ -0,0 +1,12 @@
+import kivy
+kivy.require('2.3.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyGameApp(App):
+    def build(self):
+        return Label(text='Hello World')
+    
+if __name__ == '__main__':
+    MyGameApp().run()
