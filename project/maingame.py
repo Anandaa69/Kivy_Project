0be0e4855fef9e2e1b0d7fc17709ba2f5a233daa
Anandaa69 <anandasinlapachote0@gmail.com>
2025-01-13 15:42:05 +0700

    make mainmenu and button to game

--- project/maingame.py
+++ project/maingame.py
@@ -3,10 +3,18 @@ kivy.require('2.3.1')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
+# Main Menu Game!
+class MainMenu(BoxLayout):
+    #On press botton --> clear widgets add start game
+    def start_game(self):
+        app = App.get_running_app()
+        app.root.clear_widgets()
+        app.root.add_widget(Label(text="Game Started!", font_size=24))
 class MyGameApp(App):
     def build(self):
-        return Label(text='Hello World')
+        return MainMenu()
     
 if __name__ == '__main__':
     MyGameApp().run()
