c211d98fc68855ab5de3e643af64c097e9259a77
Ananda Sinlapachot <anandasinlapachote0@gmail.com>
2025-01-14 10:30:25 +0700

    add keyboard detect

--- project/main.py
+++ project/main.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
 
 # Main Menu
@@ -17,7 +18,18 @@ class GameScreen(Screen):
 
 #Player
 class Player(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self, self._on_keyboard_closed)
+        self._keyboard.bind(on_key_down=self._on_key_down)
+    
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard = None
+        
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        print("key down has been detected")
+        
 
 
 # Main App
